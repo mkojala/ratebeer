7f07f4372aed6091e655b4a3ed6fcdbfbc149735
mkojala <mkojala@cs.helsinki.fi>
2016-02-04 13:41:56 +0200

    viikko 3 alku

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,8 +1,10 @@
 class Beer < ActiveRecord::Base
-include RatingAverage
+ include RatingAverage
  belongs_to :brewery
  has_many :ratings, dependent: :destroy
-
+ has_many :raters, through: :ratings, source: :user
+ validates :name, presence: true
+ 
 	def to_s
 		"#{name} - #{brewery.name}"
 	end
--- app/models/concerns/rating_average.rb
+++ app/models/concerns/rating_average.rb
@@ -2,11 +2,8 @@ module RatingAverage
 extend ActiveSupport::Concern
 
 	def average_rating
-		sum = 0
-		ratings.each do |r| 	
-			sum = sum + r.score
-		end
-		sum / ratings.count.to_f
+  	  return 0 if ratings.empty?
+   	  ratings.map{ |r| r.score }.sum / ratings.count.to_f
 	end
 
 end
